fc493ef9228df0c3764e2e2a7a1d08b71ce4a7bd
Orbity <orbity.startup@gmail.com>
2025-08-25 09:28:15 -0300

    Fazendo a parte de contatos

--- portfolio-orbity/src/App.jsx
+++ portfolio-orbity/src/App.jsx
@@ -2,10 +2,6 @@ import { useEffect } from 'react';
 
 // src/App.jsx
 import { Home } from './Pages/Home';
-import { AboutUs } from './Components/AboutUs/AboutUs';
-import { MissionVisionValues } from './Components/MissionVisionValues/MissionVisionValues';
-import { Team } from './Components/Team/Team';
-import { Footer } from './Components/Footer/Footer';
 
 import './App.css';
 
@@ -13,10 +9,6 @@ export default function App() {
   return (
     <>
       <Home />
-      <AboutUs />
-      <MissionVisionValues />
-      <Team />
-      <Footer />
     </>
   )
 }
---
+++ portfolio-orbity/src/Components/Contact/Contact.jsx
@@ -0,0 +1,54 @@
+import styles from './Contact.module.css';
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
+
+export function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_9ptchqa",
+        "template_wtime6k",
+        form.current,
+        "YehhX61UNz8SUKQt0"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Mensagem enviada com sucesso! 🚀");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Erro ao enviar. Tente novamente.");
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className={styles.containerEmail}>
+        <div className={styles.textArea}>
+            <h3>NÃO SEJA TÍMIDO!</h3>
+            <p>Fique a vontade para entrar em contato conosco. Estamos sempre abertos para discutir novos projetos, ideias criativas ou oportunidades de fazer partes de suas visões.</p>
+        </div>
+
+        <div className={styles.formGroup}>
+            <input type="text" name="name" placeholder="Seu nome" required className="p-2 border rounded-md" />
+
+            <input type="email" name="email" placeholder="Seu email" required className="p-2 border rounded-md" />
+            
+            <textarea name="message" placeholder="Digite sua mensagem" required className="p-2 border rounded-md h-32" />
+
+            <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition" >
+                Enviar
+            </button>
+        </div>
+    </form>
+  );
+}
+
+// service_9ptchqa
+// template_wtime6k
+// YehhX61UNz8SUKQt0
